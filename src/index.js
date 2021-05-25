be6660bd01c568cb5eaf4ea45c94695b4bf5b02d
Your Name <you@example.com>
2021-05-25 17:45:31 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import DatePickers from './hw6';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DatePickers />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
